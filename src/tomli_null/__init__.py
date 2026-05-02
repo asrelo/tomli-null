1c67b92a1aa5ff310315bfaec8bb3c9011c24d42
asrelo <47759736+asrelo@users.noreply.github.com>
2026-05-02 20:24:45 +0300

    Bump version: 1.0.3 → 1.0.4

--- src/tomli_null/__init__.py
+++ src/tomli_null/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2021 Taneli Hukkinen
 
 __all__ = ("loads", "load", "TOMLDecodeError")
-__version__ = "1.0.3"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "1.0.4"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 from ._parser import TOMLDecodeError, load, loads
 
